97f6429b38a22ef6b38125c39a8feb34e409e16d
@lvaroRuiz <alvarorrgomez@gmail.com>
2021-03-22 20:39:20 +0100

    Creada WebPage de reserva y se enlaza con boton de reservar de WebPage ofertas

---
+++ js/muestraOferta.js
@@ -0,0 +1,8 @@
+//variables
+const verPrecios = document.getElementById("boton-Reservar");
+
+// clicks en ver precios
+verPrecios.addEventListener("click", () =>
+{
+    window.location.href = "../reservar.html";
+});
