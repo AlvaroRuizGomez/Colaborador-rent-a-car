734a73794e9a40e961d3f1061f55b19917abeb00
Jose Luis <josloe@gmail.com>
2021-03-29 12:17:58 +0200

    añadido ordenaado por precio y ordenado por clasevehiculo

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,7 @@ const formSchema = require("../schemas/formschema");
 const fetch = require("node-fetch");
 const session = require('express-session');
 const eta = require("eta");
+const { abbrev } = require('../schemas/formschema');
 
 const URI_BACKEND = `${process.env.URL_BACKEND}:${process.env.PORT_BACKEND}/api`;
 
@@ -63,6 +64,13 @@ router.post('/', async (req, res) => {
     }
     else
     {
+
+        // ordenar por precio
+        dataResponse.data = await OrdenarPorPrecioTotalDias(dataResponse.data);
+
+        //ordenar por clase vehiculos
+        dataResponse.data = await OrdenaPorClaseVehiculos(dataResponse.datosOrdenacion, dataResponse.data)
+
         res.render("muestraOferta", {
             "data": dataResponse.data,
             "formdata": req.body,
@@ -75,6 +83,76 @@ router.post('/', async (req, res) => {
 
     
 });
+
+
+const OrdenaPorClaseVehiculos = async (datosOrdenacion, datosVehiculos) =>
+{
+    let buffer = [];
+
+    // ordenar por claseVehiculo
+    for (let j = 0; j < datosOrdenacion.length; j++)
+    {
+        for (let i = 0; i < datosVehiculos.length; i++)
+        {
+            if (datosVehiculos[i].clasevehiculo !== datosOrdenacion[j]) continue;
+
+            buffer.push(datosVehiculos[i]);
+
+        }
+
+    }
+
+    return buffer;
+
+}
+
+const OrdenarPorPrecioTotalDias = async (datosvehiculos) => {
+    
+    let datosVehiculosOrdenados = datosvehiculos.slice();
+    let longitudDatosVehiculosOrdenados = datosVehiculosOrdenados.length;
+    let bufferDatosVehiculos = new Array(longitudDatosVehiculosOrdenados);
+
+    for (let size = 1; size < longitudDatosVehiculosOrdenados; size *= 2) 
+    {
+        for (let leftStart = 0; leftStart < longitudDatosVehiculosOrdenados; leftStart += 2 * size) 
+        {
+            let indiceIzquierda = leftStart;
+            let indiceDerecha = Math.min(indiceIzquierda + size, longitudDatosVehiculosOrdenados);
+            let limiteIzquierda = indiceDerecha;
+            let limiteDerecha = Math.min(indiceDerecha + size, longitudDatosVehiculosOrdenados);
+            let i = indiceIzquierda;
+
+            while (indiceIzquierda < limiteIzquierda && indiceDerecha < limiteDerecha) 
+            {
+                if (datosVehiculosOrdenados[indiceIzquierda].preciototaldias <= datosVehiculosOrdenados[indiceDerecha].preciototaldias)
+                {
+                    bufferDatosVehiculos[i++] = datosVehiculosOrdenados[indiceIzquierda++];
+                }
+                else
+                {
+                    bufferDatosVehiculos[i++] = datosVehiculosOrdenados[indiceDerecha++];
+                }
+            }
+            
+            while (indiceIzquierda < limiteIzquierda) 
+            {
+                bufferDatosVehiculos[i++] = datosVehiculosOrdenados[indiceIzquierda++];
+            }
+            
+            while (indiceDerecha < limiteDerecha) {
+                bufferDatosVehiculos[i++] = datosVehiculosOrdenados[indiceDerecha++];
+            }
+        }
+
+        let temp = datosVehiculosOrdenados;
+        datosVehiculosOrdenados = bufferDatosVehiculos,
+        bufferDatosVehiculos = temp;
+    }
+
+    return datosVehiculosOrdenados;
+}
+
+
 
 
 const ControlSchema = async (body) => {
