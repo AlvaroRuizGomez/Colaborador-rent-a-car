ae403ecdc4285dffbf47dce540f418fe3643f3c7
jose luis <josloe@gmail.com>
2021-06-03 16:39:24 +0200

    input seleccionacion color con variable, actualizado mensaje error, actualizado localizacion

--- public/js/index.js
+++ public/js/index.js
@@ -11,33 +11,10 @@ for (let i = 0; i < cards.length; i++)
     cards[i].addEventListener("click", async (evento) =>
     {
         evento.preventDefault();
-        console.log("hkdjfdgf");
         
         const selectedVehiculo = cards[i].lastElementChild.lastElementChild.lastElementChild.value;
         const formulario = document.getElementById(selectedVehiculo);
         formulario.submit();
-
-        // const location = document.getElementById("idioma_directo").value;
-        // const success = document.getElementById("success_directo").value;
-        // const fase = document.getElementById("fase_directo").value;
-
-        // const body = {
-        //     "idioma": location,
-        //     "success": success,
-        //     "fase": fase,
-        //     "vehiculo": selectedVehiculo
-        // };
-
-        // const responseRaw = await fetch("/direct", {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     },
-        //     credentials: "include",
-        //     body: JSON.stringify(body)
-        // });
-        
-        // const dataResponse = await responseRaw.json();
 
     });
 
