55f793f16f19ccc0944e5600590c752f31c07f01
jose luis <josloe@gmail.com>
2021-08-17 19:36:46 +0200

    /a /b

--- src/server.js
+++ src/server.js
@@ -13,7 +13,8 @@ exports.InitServer = async () => {
     const rateLimit = require('express-rate-limit');
     const router = require('./routes/route');
     const path = require("path");
-    
+    const contentSecurityPolicy = require("express-csp-generator");
+
     const app = express();
     
     const apiLimiter = rateLimit({
@@ -69,115 +70,181 @@ exports.InitServer = async () => {
     
     // // Content-Security-Policy
     
-    app.use(
-        helmet.contentSecurityPolicy(
-        {
-            useDefaults: false,
-            directives: {
-                reportUri: ["https://gate.rapidsec.net/g/r/csp/616afffa-f826-4461-85c3-941ee6973aff/0/0/3?sct=30c0d50e-2191-4857-9e5d-aed703100472&dpos=report"],
-                baseUri: ["'self'"],
-                connectSrc: ["'self'"],
-                fontSrc: ["'self'"],
-                manifestSrc: ["'self'"],
-                mediaSrc: ["'self'"],
-                defaultSrc: ["'none'"],
-                imgSrc: [
-                    "'self'",
-                    "data:",
-                ],
-                objectSrc: ["'none'"],
-                styleSrc: [
-                    "'self'",
-                    "'unsafe-hashes'",
-                    "https://sis-t.redsys.es:25443/sis/NC/sandbox/redsysV2.js",
-                    "https://sis.redsys.es/sis/NC/redsysV2.js",
-                    "'sha256-QhTxUtlV491XQZHnTX/iZgDCdfTbN1vAILK4yt3jgYI='",
-                    "'sha256-7QjiizGaIV/0HdTo3IYJW3cdwZC5lF69KZWWFmTz8Gw='",
-                    "'unsafe-inline'"
+    // app.use(
+    //     helmet.contentSecurityPolicy(
+    //     {
+    //         useDefaults: false,
+    //         directives: {
+    //             reportUri: ["https://gate.rapidsec.net/g/r/csp/616afffa-f826-4461-85c3-941ee6973aff/0/0/3?sct=30c0d50e-2191-4857-9e5d-aed703100472&dpos=report"],
+    //             baseUri: ["'self'"],
+    //             connectSrc: ["'self'"],
+    //             fontSrc: ["'self'"],
+    //             manifestSrc: ["'self'"],
+    //             mediaSrc: ["'self'"],
+    //             defaultSrc: ["'none'"],
+    //             imgSrc: [
+    //                 "'self'",
+    //                 "data:",
+    //             ],
+    //             objectSrc: ["'none'"],
+    //             styleSrc: [
+    //                 "'self'",
+    //                 "'unsafe-hashes'",
+    //                 "https://sis-t.redsys.es:25443/sis/NC/sandbox/redsysV2.js",
+    //                 "https://sis.redsys.es/sis/NC/redsysV2.js",
+    //                 "'sha256-QhTxUtlV491XQZHnTX/iZgDCdfTbN1vAILK4yt3jgYI='",
+    //                 "'sha256-7QjiizGaIV/0HdTo3IYJW3cdwZC5lF69KZWWFmTz8Gw='",
+    //                 "'unsafe-inline'"
 
-                ],
-                styleSrcAttr: [
-                    "'self'",
-                    "'unsafe-hashes'",
-                    "https://sis-t.redsys.es:25443/sis/NC/sandbox/redsysV2.js",
-                    "https://sis.redsys.es/sis/NC/redsysV2.js",
-                    "'sha256-QhTxUtlV491XQZHnTX/iZgDCdfTbN1vAILK4yt3jgYI='",
-                    "'sha256-7QjiizGaIV/0HdTo3IYJW3cdwZC5lF69KZWWFmTz8Gw='",
-                    "https://sis-t.redsys.es:25443/",
-                    "https://sis.redsys.es",
-                    "'unsafe-inline'"
+    //             ],
+    //             styleSrcAttr: [
+    //                 "'self'",
+    //                 "'unsafe-hashes'",
+    //                 "https://sis-t.redsys.es:25443/sis/NC/sandbox/redsysV2.js",
+    //                 "https://sis.redsys.es/sis/NC/redsysV2.js",
+    //                 "'sha256-QhTxUtlV491XQZHnTX/iZgDCdfTbN1vAILK4yt3jgYI='",
+    //                 "'sha256-7QjiizGaIV/0HdTo3IYJW3cdwZC5lF69KZWWFmTz8Gw='",
+    //                 "https://sis-t.redsys.es:25443/",
+    //                 "https://sis.redsys.es",
+    //                 "'unsafe-inline'"
 
-                ],
-                formAction: ["'self'"],
-                workerSrc: ["'self'"],
-                scriptSrc: [
-                    "'self'",
-                    "https://www.google.com/maps/embed",
-                    "https://maps.googleapis.com/maps/api/js/QuotaService.RecordEvent",
-                    "https://sis-t.redsys.es:25443/sis/NC/sandbox/redsysV2.js",
-                    "https://sis.redsys.es/sis/NC/redsysV2.js",
-                    "https://sis-t.redsys.es:25443",
-                    "https://sis.redsys.es",
-                    "'sha256-S+X8s301GQoAcOI+8hj231fxePS+QG94YC0px7AraoQ='",
-                    "'sha256-Z6AFHJcDDPHLaWVdLcifmBDDzjDMApb7nM2qbkPTJeo='",
-                    "'sha256-YVCjXA9IbEbK3w4jDiqhWcfCPs+7VhG2TuPyX8v/NB8='",
-                    "'sha256-V7W7QXt7q8HLNfscNP1nsRRepxg+sOz7CfsY1z6EzfY='",
-                    "'sha256-iGoNEAX01mPhFHPIHqILK51hg3IxPfVSI9LFcd/8Vpg='",
-                    "'sha256-PZEg+mIdptYTwWmLcBTsa99GIDZujyt7VHBZ9Lb2Jys='",
-                    "'sha256-c7UrMTK2hnfEDAZh1ENjqCJcH/9cqaKMEMPjIE9RUFM='",
-                    "'sha256-5YNcDGqyZeDxGr9YmU6qLTlPX3Cgq14oFpH7CX5CXgM='",
-                    "'sha256-tSulbyIC9pCfjTMSJ+oGN0txgCAxkNMdf3mNyhvqLd8='",
+    //             ],
+    //             formAction: ["'self'"],
+    //             workerSrc: ["'self'"],
+    //             scriptSrc: [
+    //                 "'self'",
+    //                 "https://www.google.com/maps/embed",
+    //                 "https://maps.googleapis.com/maps/api/js/QuotaService.RecordEvent",
+    //                 "https://sis-t.redsys.es:25443/sis/NC/sandbox/redsysV2.js",
+    //                 "https://sis.redsys.es/sis/NC/redsysV2.js",
+    //                 "https://sis-t.redsys.es:25443",
+    //                 "https://sis.redsys.es",
+    //                 "'sha256-S+X8s301GQoAcOI+8hj231fxePS+QG94YC0px7AraoQ='",
+    //                 "'sha256-Z6AFHJcDDPHLaWVdLcifmBDDzjDMApb7nM2qbkPTJeo='",
+    //                 "'sha256-YVCjXA9IbEbK3w4jDiqhWcfCPs+7VhG2TuPyX8v/NB8='",
+    //                 "'sha256-V7W7QXt7q8HLNfscNP1nsRRepxg+sOz7CfsY1z6EzfY='",
+    //                 "'sha256-iGoNEAX01mPhFHPIHqILK51hg3IxPfVSI9LFcd/8Vpg='",
+    //                 "'sha256-PZEg+mIdptYTwWmLcBTsa99GIDZujyt7VHBZ9Lb2Jys='",
+    //                 "'sha256-c7UrMTK2hnfEDAZh1ENjqCJcH/9cqaKMEMPjIE9RUFM='",
+    //                 "'sha256-5YNcDGqyZeDxGr9YmU6qLTlPX3Cgq14oFpH7CX5CXgM='",
+    //                 "'sha256-tSulbyIC9pCfjTMSJ+oGN0txgCAxkNMdf3mNyhvqLd8='",
                     
-                    "'unsafe-inline'",
-                ],
-                scriptSrcElem: [
-                    "'self'", 
-                    "https://sis-t.redsys.es:25443",
-                    "https://sis.redsys.es",
-                    "https://www.google.com/maps/embed",
-                    "https://maps.googleapis.com/maps/api/js/QuotaService.RecordEvent",
-                    "https://sis-t.redsys.es:25443/sis/NC/sandbox/redsysV2.js",
-                    "https://sis.redsys.es/sis/NC/redsysV2.js",
-                    "'sha256-S+X8s301GQoAcOI+8hj231fxePS+QG94YC0px7AraoQ='",
-                    "'sha256-Z6AFHJcDDPHLaWVdLcifmBDDzjDMApb7nM2qbkPTJeo='",
-                    "'sha256-YVCjXA9IbEbK3w4jDiqhWcfCPs+7VhG2TuPyX8v/NB8='",
-                    "'sha256-V7W7QXt7q8HLNfscNP1nsRRepxg+sOz7CfsY1z6EzfY='",
-                    "'sha256-iGoNEAX01mPhFHPIHqILK51hg3IxPfVSI9LFcd/8Vpg='",
-                    "'sha256-PZEg+mIdptYTwWmLcBTsa99GIDZujyt7VHBZ9Lb2Jys='",
-                    "'sha256-c7UrMTK2hnfEDAZh1ENjqCJcH/9cqaKMEMPjIE9RUFM='",
-                    "'sha256-5YNcDGqyZeDxGr9YmU6qLTlPX3Cgq14oFpH7CX5CXgM='",
-                    "'sha256-tSulbyIC9pCfjTMSJ+oGN0txgCAxkNMdf3mNyhvqLd8='"
+    //                 "'unsafe-inline'",
+    //             ],
+    //             scriptSrcElem: [
+    //                 "'self'", 
+    //                 "https://sis-t.redsys.es:25443",
+    //                 "https://sis.redsys.es",
+    //                 "https://www.google.com/maps/embed",
+    //                 "https://maps.googleapis.com/maps/api/js/QuotaService.RecordEvent",
+    //                 "https://sis-t.redsys.es:25443/sis/NC/sandbox/redsysV2.js",
+    //                 "https://sis.redsys.es/sis/NC/redsysV2.js",
+    //                 "'sha256-S+X8s301GQoAcOI+8hj231fxePS+QG94YC0px7AraoQ='",
+    //                 "'sha256-Z6AFHJcDDPHLaWVdLcifmBDDzjDMApb7nM2qbkPTJeo='",
+    //                 "'sha256-YVCjXA9IbEbK3w4jDiqhWcfCPs+7VhG2TuPyX8v/NB8='",
+    //                 "'sha256-V7W7QXt7q8HLNfscNP1nsRRepxg+sOz7CfsY1z6EzfY='",
+    //                 "'sha256-iGoNEAX01mPhFHPIHqILK51hg3IxPfVSI9LFcd/8Vpg='",
+    //                 "'sha256-PZEg+mIdptYTwWmLcBTsa99GIDZujyt7VHBZ9Lb2Jys='",
+    //                 "'sha256-c7UrMTK2hnfEDAZh1ENjqCJcH/9cqaKMEMPjIE9RUFM='",
+    //                 "'sha256-5YNcDGqyZeDxGr9YmU6qLTlPX3Cgq14oFpH7CX5CXgM='",
+    //                 "'sha256-tSulbyIC9pCfjTMSJ+oGN0txgCAxkNMdf3mNyhvqLd8='"
                     
-                ],
-                childSrc: [
-                    "'self'",
-                    "https://apis.google.com",
-                    "https://maps.googleapis.com",
-                    "https://www.google.com", 
-                    "https://sis-t.redsys.es:25443/"
-                ],
-                frameSrc: [
-                    "'self'",
-                    "https://apis.google.com", 
-                    "https://maps.googleapis.com", 
-                    "https://www.google.com", 
-                    "https://sis-t.redsys.es:25443/",
-                    "https://sis.redsys.es/",
-                ],
+    //             ],
+    //             childSrc: [
+    //                 "'self'",
+    //                 "https://apis.google.com",
+    //                 "https://maps.googleapis.com",
+    //                 "https://www.google.com", 
+    //                 "https://sis-t.redsys.es:25443/"
+    //             ],
+    //             frameSrc: [
+    //                 "'self'",
+    //                 "https://apis.google.com", 
+    //                 "https://maps.googleapis.com", 
+    //                 "https://www.google.com", 
+    //                 "https://sis-t.redsys.es:25443/",
+    //                 "https://sis.redsys.es/",
+    //             ],
                 
-                frameAncestors: [
-                    "'self'",
-                    "https://apis.google.com",
-                    "https://maps.googleapis.com",
-                    "https://www.google.com",
-                    "https://sis-t.redsys.es:25443/",
-                    "https://sis.redsys.es/",
+    //             frameAncestors: [
+    //                 "'self'",
+    //                 "https://apis.google.com",
+    //                 "https://maps.googleapis.com",
+    //                 "https://www.google.com",
+    //                 "https://sis-t.redsys.es:25443/",
+    //                 "https://sis.redsys.es/",
+    //             ],
+    //             upgradeInsecureRequests: [],
+    //             blockAllMixedContent: [],
+    //         },
+    //     })
+    // );
+
+
+    // ------------------------
+    app.use(
+        contentSecurityPolicy({
+            "directives": {
+                "frame-ancestors": [
+                    "'self'"
                 ],
-                upgradeInsecureRequests: [],
-                blockAllMixedContent: [],
+                "block-all-mixed-content": [],
+                "default-src": [
+                    "'self'"
+                ],
+                "script-src": [
+                    "'self'",
+                    "'report-sample'"
+                ],
+                "style-src": [
+                    "'self'",
+                    "'report-sample'"
+                ],
+                "object-src": [
+                    "'none'"
+                ],
+                "frame-src": [
+                    "'self'"
+                ],
+                "child-src": [
+                    "'self'"
+                ],
+                "img-src": [
+                    "'self'"
+                ],
+                "font-src": [
+                    "'self'"
+                ],
+                "connect-src": [
+                    "'self'"
+                ],
+                "manifest-src": [
+                    "'self'"
+                ],
+                "base-uri": [
+                    "'self'"
+                ],
+                "form-action": [
+                    "'self'"
+                ],
+                "media-src": [
+                    "'self'"
+                ],
+                "prefetch-src": [
+                    "'self'"
+                ],
+                "worker-src": [
+                    "'self'"
+                ],
+                "report-uri": [
+                    "https://gate.rapidsec.net/g/r/csp/616afffa-f826-4461-85c3-941ee6973aff/0/2/3?sct=30c0d50e-2191-4857-9e5d-aed703100472&dpos=report"
+                ]
             },
+            "reportOnly": true
         })
     );
+
+    //------------
 
     app.use(helmet.dnsPrefetchControl());
     app.use(helmet.expectCt());
