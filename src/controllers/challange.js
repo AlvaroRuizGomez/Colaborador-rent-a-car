4ae81964d57629d0f6e7be7f4791c15b78f08c82
jose luis <2064537+sosan@users.noreply.github.com>
2021-10-27 00:21:47 +0200

    /a /b

--- src/controllers/challange.js
+++ src/controllers/challange.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 exports.URIChallange = async (req, res) => {
-    console.log("acme challange url=" + JSON.stringify(req));
+    // console.log("acme challange url=" + JSON.stringify(req));
 
     if (req.url.indexOf("/.well-known/acme-challenge/") === -1)
     {
