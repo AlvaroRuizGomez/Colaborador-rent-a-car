05aefec2aff6af8cd65ca2963632d5cccdb6d5cd
jose luis <2064537+sosan@users.noreply.github.com>
2021-10-26 22:49:07 +0200

    /a /b

--- src/controllers/challange.js
+++ src/controllers/challange.js
@@ -1,14 +1,19 @@
 const path = require("path");
 
-exports.URIChallangeFirstPart = async (req, res) =>
-{
-    res.sendFile(path.join(__dirname, "../../public/.well-known/acme-challange/parteuno"));
-    
+exports.URIChallange = async (req, res) => {
+    console.log("acme challange url" + req.url);
+
+    if (req.url.indexOf("/.well-known/acme-challenge/") === -1)
+    {
+        return res.send("");
+    }
+
+    const url = req.url.split("/.well-known/acme-challenge/")[1];
+    return res.send(url);
+    // return res.sendFile(path.join(__dirname, "../../public/.well-known/acme-challange/parteuno"));
+
 };
 
-exports.URIChallangeSecondPart = async (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public/.well-known/acme-challange/partedos"));
 
-};
 
 
